a5b7dc45e87da6877f83814072b29a945690ab61
FinKedinn1 <finkedinnze@gmail.com>
2026-04-14 18:29:27 -0300

    Agregamos el endpoint post de usuarios

--- routes/usuarios.py
+++ routes/usuarios.py
@@ -17,6 +17,42 @@ def listar_usuarios():
         })
     return jsonify(resultado), 200
 
+
+@usuarios_crear_bp.route("/usuarios", methods=["POST"])
+def crear_usuario():
+    data = request.get_json()  #Lee lo que manda el cliente
+
+    if not data:
+        return jsonify({"error": "No se enviaron datos"}), 400
+
+    campos_obligatorios = ["nombre", "email"]
+
+    for campo in campos_obligatorios:
+        if campo not in data:
+            return jsonify({"error": f"Falta el campo {campo}"}), 400
+    
+    if "@" not in data["email"]:  #Valida el email
+                return jsonify({"error":"Email invalido"}), 400
+
+    nuevo_usuario = Usuario(
+        nombre=data["nombre"],
+        email=data["email"]
+    )
+
+
+    db.session.add(nuevo_usuario)  #Lo prepara para guardarlo en la base de datos
+    db.session.commit()  #Lo guarda 
+
+
+    return jsonify({
+        "mensaje": "Usuario creado correctamente",
+        "usuario": {
+            "id": nuevo_usuario.id,
+            "nombre": nuevo_usuario.nombre,
+            "email": nuevo_usuario.email
+        }
+    }), 201
+    
 @usuarios_bp.route("/usuarios", methods=["POST"])
 def crear_usuario():
     data = request.get_json()
