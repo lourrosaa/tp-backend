b0f5da2065e5d1c4fa3ffaf1a6ec5542886c9662
Lara Ovejero <lovejero@fi.uba.ar>
2026-04-17 21:14:50 -0300

    arregle cambios finales para el funcionamiento

--- app.py
+++ app.py
@@ -1,21 +1,25 @@
 from flask import Flask
-from routes.partidos import partidos_bp
-from routes.usuarios import usuarios_id_bp
+from config import Config
 from database import db
+from routes.partidos import partidos_bp
+from routes.usuarios import usuarios_bp
+from routes.ranking import ranking_bp
 
 app = Flask(__name__)
+app.register_blueprint(partidos_bp)
+app.register_blueprint(usuarios_bp)
+app.register_blueprint(ranking_bp)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:root@localhost/TP_BACKEND"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config.from_object(Config)
 
 db.init_app(app)
+
+with app.app_context():
+    db.create_all()
 
 @app.route("/")
 def home():
     return "API funcionando"
-
-app.register_blueprint(partidos_bp)
-app.register_blueprint(usuarios_id_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
